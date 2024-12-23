ad4f7b8905b520ae7bdbbe267ac5f538278e3454
Chengyu Yang <2160135002@qq.com>
2024-12-23 08:48:26 +0800

    1

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ geodata=requests.get(geourl).text
 df_abb=pd.read_csv("4country_abb.csv")
 dic_abb = {df_abb.iloc[i, 1]: df_abb.iloc[i, 0] for i in range(np.shape(df_abb)[0])}
 covid19_df['fullname'] = covid19_df['Country'].map(dic_abb)
+covid19_df['log_Cumulative_Confirmed']=np.log(covid19_df['Cumulative Confirmed']+1)
 folium.Choropleth(geo_data=geodata, name="covid-19 total confirm map", data=covid19_df,
-                  columns=["fullname", "Cumulative Confirmed"], key_on="feature.properties.name",
+                  columns=["fullname", "log_Cumulative_Confirmed"], key_on="feature.properties.name",
                   fill_color='PuRd', nan_fill_color='white').add_to(m)
 folium.map.Marker(
-    location=[0, 0],  # 修改为您希望显示的具体位置
-    icon=folium.DivIcon(html='<div style="font-size: 24px; color: red;">杨承宇 6020224625</div>')  # 切换为您的姓名
+    location=[0, 0],  # 具体位置
+    icon=folium.DivIcon(html='<div style="font-size: 24px; color: red;">杨承宇 6020224625</div>')  # 姓名信息
 ).add_to(m)
 m.save("2.html")
